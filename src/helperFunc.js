8fdd6d8835abfb995b050c453888fb14cbf45083
Yurii <jurabartkiv@gmail.com>
2024-10-08 19:21:59 -0500

    sdf

--- src/helperFunc.js
+++ src/helperFunc.js
@@ -1,45 +1,45 @@
-export const backgroundChanger = (weather) => {
-  if (weather.current?.condition.text === "Overcast") {
-    return "/weatherApp/src/assets/sky-1107579_1920.webp";
-  } else if (weather.current?.condition.text === "Partly cloudy") {
-    return "/weatherApp/src/assets/PartlyCloudy2.webp";
-  } else if (weather.current?.condition.text === "Mist") {
-    return "/weatherApp/src/assets/mist.jpg";
-  } else if (weather.current?.condition.text === "Sunny") {
-    return "/weatherApp/src/assets/Sunny.jpeg";
-  } else if (weather.current?.condition.text === "Clear") {
-    return "/weatherApp/src/assets/clear.jpg";
-  } else if (weather.current?.condition.text === "Moderate rain") {
-    return "/weatherApp/src/assets/moderateRain.webp";
-  } else if (weather.current?.condition.text === "Torrential rain shower") {
-    return "/weatherApp/src/assets/rainShower.jpg";
-  } else if (weather.current?.condition.text === "Partly Cloudy") {
-    return "/weatherApp/src/assets/PartlyCloudy2.webp";
-  } else if (weather.current?.condition.text === "Patchy rain nearby") {
-    return "/weatherApp/src/assets/moderateRain.webp";
-  }else if (weather.current?.condition.text === "Light rain") {
-    return "/weatherApp/src/assets/moderateRain.webp";
-  }
-};
-
 // export const backgroundChanger = (weather) => {
 //   if (weather.current?.condition.text === "Overcast") {
-//     return "/src/assets/sky-1107579_1920.webp";
+//     return "/weatherApp/src/assets/sky-1107579_1920.webp";
 //   } else if (weather.current?.condition.text === "Partly cloudy") {
-//     return "/src/assets/PartlyCloudy2.webp";
+//     return "/weatherApp/src/assets/PartlyCloudy2.webp";
 //   } else if (weather.current?.condition.text === "Mist") {
-//     return "/src/assets/mist.jpg";
+//     return "/weatherApp/src/assets/mist.jpg";
 //   } else if (weather.current?.condition.text === "Sunny") {
-//     return "/src/assets/Sunny.jpeg";
+//     return "/weatherApp/src/assets/Sunny.jpeg";
 //   } else if (weather.current?.condition.text === "Clear") {
-//     return "/src/assets/clear.jpg";
+//     return "/weatherApp/src/assets/clear.jpg";
 //   } else if (weather.current?.condition.text === "Moderate rain") {
-//     return "/src/assets/moderateRain.webp";
+//     return "/weatherApp/src/assets/moderateRain.webp";
 //   } else if (weather.current?.condition.text === "Torrential rain shower") {
-//     return "/src/assets/rainShower.jpg";
+//     return "/weatherApp/src/assets/rainShower.jpg";
 //   } else if (weather.current?.condition.text === "Partly Cloudy") {
-//     return "/src/assets/PartlyCloudy2.webp";
+//     return "/weatherApp/src/assets/PartlyCloudy2.webp";
 //   } else if (weather.current?.condition.text === "Patchy rain nearby") {
-//     return "/src/assets/moderateRain.webp";
+//     return "/weatherApp/src/assets/moderateRain.webp";
+//   }else if (weather.current?.condition.text === "Light rain") {
+//     return "/weatherApp/src/assets/moderateRain.webp";
 //   }
 // };
+
+export const backgroundChanger = (weather) => {
+  if (weather.current?.condition.text === "Overcast") {
+    return "./assets/sky-1107579_1920.webp";
+  } else if (weather.current?.condition.text === "Partly cloudy") {
+    return "./assets/PartlyCloudy2.webp";
+  } else if (weather.current?.condition.text === "Mist") {
+    return "./assets/mist.jpg";
+  } else if (weather.current?.condition.text === "Sunny") {
+    return "./assets/Sunny.jpeg";
+  } else if (weather.current?.condition.text === "Clear") {
+    return "./assets/clear.jpg";
+  } else if (weather.current?.condition.text === "Moderate rain") {
+    return "./assets/moderateRain.webp";
+  } else if (weather.current?.condition.text === "Torrential rain shower") {
+    return "./assets/rainShower.jpg";
+  } else if (weather.current?.condition.text === "Partly Cloudy") {
+    return "./assets/PartlyCloudy2.webp";
+  } else if (weather.current?.condition.text === "Patchy rain nearby") {
+    return "./assets/moderateRain.webp";
+  }
+};
