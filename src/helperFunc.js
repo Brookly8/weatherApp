6ef211d16c5f4b0d7630205effbaa6e0c1018852
Yurii <jurabartkiv@gmail.com>
2024-10-08 19:46:41 -0500

    ewq

--- src/helperFunc.js
+++ src/helperFunc.js
@@ -1,23 +1,23 @@
 export const backgroundChanger = (weather) => {
   if (weather.current?.condition.text === "Overcast") {
-    return "/assets/sky-1107579_1920.webp";
+    return "weatherApp/public/assets/sky-1107579_1920.webp";
   } else if (weather.current?.condition.text === "Partly cloudy") {
-    return "/assets/PartlyCloudy2.webp";
+    return "weatherApp/public/assets/PartlyCloudy2.webp";
   } else if (weather.current?.condition.text === "Mist") {
-    return "/assets/mist.jpg";
+    return "weatherApp/public/assets/mist.jpg";
   } else if (weather.current?.condition.text === "Sunny") {
-    return "/assets/Sunny.jpeg";
+    return "weatherApp/public/assets/Sunny.jpeg";
   } else if (weather.current?.condition.text === "Clear") {
-    return "/assets/clear.jpg";
+    return "weatherApp/public/assets/clear.jpg";
   } else if (weather.current?.condition.text === "Moderate rain") {
-    return "/assets/moderateRain.webp";
+    return "weatherApp/public/assets/moderateRain.webp";
   } else if (weather.current?.condition.text === "Torrential rain shower") {
-    return "/assets/rainShower.jpg";
+    return "weatherApp/public/assets/rainShower.jpg";
   } else if (weather.current?.condition.text === "Partly Cloudy") {
-    return "/assets/PartlyCloudy2.webp";
+    return "weatherApp/public/assets/PartlyCloudy2.webp";
   } else if (weather.current?.condition.text === "Patchy rain nearby") {
-    return "/assets/moderateRain.webp";
+    return "weatherApp/public/assets/moderateRain.webp";
   } else if (weather.current?.condition.text === "Light rain") {
-    return "/assets/moderateRain.webp";
+    return "weatherApp/public/assets/moderateRain.webp";
   }
 };
