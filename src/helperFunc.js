5f8e69850a72e317fd516ce0f0ebe77a11ff293f
Yurii <jurabartkiv@gmail.com>
2024-10-08 19:42:06 -0500

    de

--- src/helperFunc.js
+++ src/helperFunc.js
@@ -1,45 +1,23 @@
 export const backgroundChanger = (weather) => {
   if (weather.current?.condition.text === "Overcast") {
-    return "/weatherApp/src/assets/sky-1107579_1920.webp";
+    return "/assets/sky-1107579_1920.webp";
   } else if (weather.current?.condition.text === "Partly cloudy") {
-    return "/weatherApp/src/assets/PartlyCloudy2.webp";
+    return "/assets/PartlyCloudy2.webp";
   } else if (weather.current?.condition.text === "Mist") {
-    return "/weatherApp/src/assets/mist.jpg";
+    return "/assets/mist.jpg";
   } else if (weather.current?.condition.text === "Sunny") {
-    return "/weatherApp/src/assets/Sunny.jpeg";
+    return "/assets/Sunny.jpeg";
   } else if (weather.current?.condition.text === "Clear") {
-    return "/weatherApp/src/assets/clear.jpg";
+    return "/assets/clear.jpg";
   } else if (weather.current?.condition.text === "Moderate rain") {
-    return "/weatherApp/src/assets/moderateRain.webp";
+    return "/assets/moderateRain.webp";
   } else if (weather.current?.condition.text === "Torrential rain shower") {
-    return "/weatherApp/src/assets/rainShower.jpg";
+    return "/assets/rainShower.jpg";
   } else if (weather.current?.condition.text === "Partly Cloudy") {
-    return "/weatherApp/src/assets/PartlyCloudy2.webp";
+    return "/assets/PartlyCloudy2.webp";
   } else if (weather.current?.condition.text === "Patchy rain nearby") {
-    return "/weatherApp/src/assets/moderateRain.webp";
-  }else if (weather.current?.condition.text === "Light rain") {
-    return "/weatherApp/src/assets/moderateRain.webp";
+    return "/assets/moderateRain.webp";
+  } else if (weather.current?.condition.text === "Light rain") {
+    return "/assets/moderateRain.webp";
   }
 };
-
-// export const backgroundChanger = (weather) => {
-//   if (weather.current?.condition.text === "Overcast") {
-//     return "./assets/sky-1107579_1920.webp";
-//   } else if (weather.current?.condition.text === "Partly cloudy") {
-//     return "./assets/PartlyCloudy2.webp";
-//   } else if (weather.current?.condition.text === "Mist") {
-//     return "./assets/mist.jpg";
-//   } else if (weather.current?.condition.text === "Sunny") {
-//     return "./assets/Sunny.jpeg";
-//   } else if (weather.current?.condition.text === "Clear") {
-//     return "./assets/clear.jpg";
-//   } else if (weather.current?.condition.text === "Moderate rain") {
-//     return "./assets/moderateRain.webp";
-//   } else if (weather.current?.condition.text === "Torrential rain shower") {
-//     return "./assets/rainShower.jpg";
-//   } else if (weather.current?.condition.text === "Partly Cloudy") {
-//     return "./assets/PartlyCloudy2.webp";
-//   } else if (weather.current?.condition.text === "Patchy rain nearby") {
-//     return "./assets/moderateRain.webp";
-//   }
-// };
